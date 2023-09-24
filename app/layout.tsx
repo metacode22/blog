25b40777cb45f62c646a2de199b918ee9e855663
metacode22 <alohajune22@gmail.com>
2023-09-24 17:43:09 +0900

    design: layout 디자인 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col items-center`}>
         <GlobalNavigationBar />
-        <main className='flex justify-center'>
-          <div className='max-w-5xl border-red-100 border-2 w-full p-4'>{children}</div>
-        </main>
+        <main className='w-full max-w-5xl p-4'>{children}</main>
       </body>
     </html>
   );
