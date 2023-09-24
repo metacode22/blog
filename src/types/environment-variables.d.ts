08b8830751deaf8ccffccee6b782569c4a1d006a
metacode22 <alohajune22@gmail.com>
2023-09-24 17:42:48 +0900

    chore: environment-variables.d.ts 파일에 타입 추가

--- src/types/environment-variables.d.ts
+++ src/types/environment-variables.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    readonly NEXT_PUBLIC_POSTS_SOURCE: string;
+
+    // server only
+    readonly GITHUB_API_URL_TO_GET_POSTS_SOURCE_TREE: string;
     readonly GITHUB_TOKEN: string;
   }
 }
