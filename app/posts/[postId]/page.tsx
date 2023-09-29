3d2dd749c33b4dbdd222d1ac7bf8c97896e00da5
metacode22 <alohajune22@gmail.com>
2023-09-29 17:00:54 +0900

    feat: post detail의 post head 부분 구현

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -18,14 +18,24 @@ export default async function PostDetailPage({
     <article className='prose flex w-full max-w-none flex-col'>
       <div className='flex w-full justify-center bg-gradient-to-b from-indigo-50 px-4 py-8'>
         <div className='w-full max-w-5xl'>
+          <div className='relative min-h-[240px] overflow-hidden'>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_POSTS_SOURCE}/${postId}/thumbnail.png`}
+              alt={`${postDetail.meta.title} 썸네일`}
+              fill
+              className='m-0 object-contain'
+            />
+          </div>
           <div className='flex flex-col items-start gap-8'>
-            {postDetail.meta.categories.map(category => (
-              <span
-                key={category}
-                className='rounded-[32px] bg-indigo-500 px-3 py-[6px] text-sm text-white'>
-                {category}
-              </span>
-            ))}
+            <div className='flex justify-start gap-4'>
+              {postDetail.meta.categories.map(category => (
+                <span
+                  key={category}
+                  className='rounded-[32px] bg-indigo-500 px-3 py-[6px] text-sm text-white'>
+                  {category}
+                </span>
+              ))}
+            </div>
             <h1 className='m-0'>{postDetail.meta.title}</h1>
             <div>
               <p className='m-0'>{postDetail.meta.summary}</p>
