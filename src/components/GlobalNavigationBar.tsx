5bf4b339b103f2704dd0258b81aebfb1e309790f
metacode22 <alohajune22@gmail.com>
2023-10-02 14:26:45 +0900

    feat: 초기 gnb 구현

--- src/components/GlobalNavigationBar.tsx
+++ src/components/GlobalNavigationBar.tsx
@@ -4,28 +4,38 @@ import { CiSearch } from 'react-icons/ci';
 
 export default function GlobalNavigationBar() {
   return (
-    <nav className='bg-white flex w-full items-center justify-between border-b-[1px] p-4 lg:px-20 lg:py-4'>
-      <ul className='flex justify-start gap-3'>
-        <li>
-          <Link href={ROUTES.HOME}>Logo</Link>
-        </li>
-        <li>
-          <Link href={ROUTES.HOME}>Home</Link>
-        </li>
-        {/**
-         * @TODO
-         * About 페이지 만들어지면 주석 해제하기
-         */}
-        {/* <li>
+    <nav className='flex w-full items-center justify-between border-b-[1px] bg-white p-4 lg:px-20 lg:py-3'>
+      <div className='flex items-center gap-10'>
+        <Link href={ROUTES.HOME} className='rounded-lg text-xl font-semibold italic'>
+          Jun&apos;s blog
+        </Link>
+        <ul className='flex justify-start gap-3 rounded-md px-2 py-1 leading-7 transition hover:bg-slate-100'>
+          <li>
+            <Link href={ROUTES.HOME} className=''>
+              Home
+            </Link>
+          </li>
+          {/**
+           * @TODO
+           * About 페이지 만들어지면 주석 해제하기
+           */}
+          {/* <li>
           <Link href={ROUTES.ABOUT}>About</Link>
         </li> */}
-      </ul>
+        </ul>
+      </div>
       <ul>
         <li>
-          <div className='flex items-center justify-end gap-3'>
-            <input type='text' className='border-2 border-teal-200' />
-            <CiSearch size={20} />
-          </div>
+          {/**
+           * @TODO
+           * 검색 기능 구현하기
+           */}
+          {/* <button className='relative h-8 w-60 rounded-2xl bg-slate-200 transition hover:bg-slate-300'>
+            <CiSearch
+              size={20}
+              className='absolute right-4 -translate-y-1/2 text-slate-600 transition hover:text-slate-900'
+            />
+          </button> */}
         </li>
       </ul>
     </nav>
