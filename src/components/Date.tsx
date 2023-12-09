3328375c4696c54689b9c56c4eff9871c1aab13c
metacode22 <alohajune22@gmail.com>
2023-12-09 22:47:44 +0900

    chore: Date 컴포넌트에 jsDoc 추가

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,3 +1,6 @@
+/**
+ * @param date - YYYY-MM-DD와 같은 형식으로 들어와야 함.
+ */
 export default function Date({ date }: { date: string }) {
   return (
     <time dateTime={date} className='text-sm text-gray-400'>
