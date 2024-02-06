08b324c5e59d1a516df083bf5f207909a910f5ba
metacode22 <alohajune22@gmail.com>
2024-02-06 23:57:33 +0900

    feat: Category에 리다이렉팅 기능 부여

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,8 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
+import ROUTES from '../constants/routes';
 
 export default function Category({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  function handleClickCategory() {
+    router.push(ROUTES.POSTS);
+  }
+
   return (
-    <span className='text-sm text-gray-500 hover:underline'>
+    <span className='text-sm text-gray-500 hover:underline' onClick={handleClickCategory}>
       {children}
     </span>
   );
