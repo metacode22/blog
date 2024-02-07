4ea66397ee790d55513980bc380246c2f93a26a3
metacode22 <alohajune22@gmail.com>
2024-02-07 23:55:55 +0900

    feat: post들을 바탕으로 categories 계산

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,13 +2,27 @@ import { allPosts } from '@/.contentlayer/generated';
 import PostList from '../components/PostList';
 import { compareDesc } from 'date-fns';
 
+type Categories = Record<string, number>;
+
 export default function Posts() {
   const posts = allPosts
     .filter(post => post.isPublished)
     .sort((a, b) => compareDesc(new Date(a.updatedAt), new Date(b.updatedAt)));
+  const categories = posts.reduce((acc, post) => {
+    post.categories.forEach(category => {
+      acc[category] = acc[category] ? acc[category] + 1 : 1;
+    });
+
+    return acc;
+  }, {} as Categories);
 
   return (
     <div className='flex w-full flex-col items-start gap-6'>
+      <div>
+        {/** 
+         * @todo categories
+         */}
+      </div>
       <PostList posts={posts} />
     </div>
   );
--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,6 +4,10 @@ import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
 import ROUTES from '../constants/routes';
 
+/**
+ * @todo query string, categories=[category1, category2]
+ * @todo router -> Link, use client -> use server
+ */
 export default function Category({ children }: { children: ReactNode }) {
   const router = useRouter();
 
