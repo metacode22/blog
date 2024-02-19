8cb6404eb4a01820f07e31b9e63434e766c1481e
metacode22 <alohajune22@gmail.com>
2024-02-19 15:40:43 +0900

    refactor: 불필요한 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import { allPosts } from 'contentlayer/generated';
 import { compareDesc } from 'date-fns';
 
-import { isEmptyArray } from '@/src/utils/array';
-
 import PostList from './components/PostList';
-import PostListItem from './components/PostListItem';
 
 export default async function HomePage() {
   const posts = allPosts
     .filter(post => post.isPublished)
     .sort((a, b) => compareDesc(new Date(a.updatedAt), new Date(b.updatedAt)));
 
-  if (!posts || !posts.length) return <div>무언가 잘못됨!</div>;
-
   return (
     <div className='flex w-full flex-col items-start'>
-      {/* {!isEmptyArray(posts) ? <PostListItem post={posts[0]} /> : null} */}
       <PostList posts={posts} />
     </div>
   );
