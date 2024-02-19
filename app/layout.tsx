953c7d13e0c45f68c50084f3318b55a1ae48df4d
metacode22 <alohajune22@gmail.com>
2024-02-19 23:20:48 +0900

    feat: google search console verification을 위한 코드 삽입

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,16 @@ import GoogleAnalyticsScript from '@/src/components/GoogleAnalyticsScript';
 export const metadata: Metadata = {
   title: '신승준 블로그',
   description: '프론트엔드 개발자 신승준의 개인 블로그입니다.',
+  verification: {
+    google: 'YP4UXQP92HR2Cwg7cRLU9g3OQ8OLVlzxNaASuYQbAzg',
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='ko'>
       <body className='flex w-full flex-col items-center'>
