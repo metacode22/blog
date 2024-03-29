1d74aef73fdd6290bf529262413f06716b514fe9
metacode22 <alohajune22@gmail.com>
2024-03-29 23:49:06 +0900

    feat: canonical url 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,12 @@ import GlobalNavigationBar from '@/src/components/GlobalNavigationBar';
 import GoogleAnalyticsScript from '@/src/components/GoogleAnalyticsScript';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.SITE_URL),
   title: '신승준 블로그',
   description: '프론트엔드 개발자 신승준의 개인 블로그입니다.',
+  alternates: {
+    canonical: './',
+  },
   verification: {
     google: 'YP4UXQP92HR2Cwg7cRLU9g3OQ8OLVlzxNaASuYQbAzg',
     other: {
