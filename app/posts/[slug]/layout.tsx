97164f965be0ccac15ec1fdc0fb546a4f65cc1d3
metacode22 <alohajune22@gmail.com>
2024-03-31 23:35:55 +0900

    feat: setup-jest-in-monorepo 글 작성

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
 export default function PostLayout({ children }: { children: ReactNode }) {
-  return <div className='max-w-3xl'>{children}</div>;
+  return <div className='w-full max-w-3xl'>{children}</div>;
 }
