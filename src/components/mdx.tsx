456828ef3057810a4d6a5c44de8c067d2dded0c8
metacode22 <alohajune22@gmail.com>
2024-08-14 16:01:17 +0900

    feat: 이미지 렌더링 방식 수정

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -3,15 +3,20 @@
 import Image, { ImageProps } from 'next/image';
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
 
-export function MDX({
-  scope = {},
-  ...props
-}: Omit<MDXRemoteProps, 'scope'> & { scope?: Record<string, any> }) {
-  return (
-    <MDXRemote {...props} scope={scope} components={{ Image: RoundedImage }} />
-  );
+export function MDX({ scope = {}, ...props }: Omit<MDXRemoteProps, 'scope'> & { scope?: Record<string, any> }) {
+  return <MDXRemote {...props} scope={scope} components={{ Image: RoundedImage }} />;
 }
 
-function RoundedImage({ alt, ...props }: ImageProps) {
-  return <Image alt={alt ?? ''} className='rounded-lg' {...props} />;
+function RoundedImage({ alt, src, ...props }: ImageProps) {
+  return (
+    <Image
+      alt={alt ?? ''}
+      src={src}
+      width={0}
+      height={0}
+      sizes='100vw'
+      className='h-auto w-full rounded-lg'
+      {...props}
+    />
+  );
 }
