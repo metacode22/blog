c0ab0f5e67c1ca75f7cb9fa87afbc72b022f579b
metacode22 <alohajune22@gmail.com>
2024-08-15 16:27:04 +0900

    feat: views response 캐싱

--- src/apis/view.ts
+++ src/apis/view.ts
@@ -1,11 +1,19 @@
+const viewsMap = new Map();
+
 export async function getViews(slug: string): Promise<{ views: number }> {
+  if (viewsMap.has(slug)) {
+    return viewsMap.get(slug);
+  }
+
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/views?slug=${slug}`);
 
   if (!response.ok) {
     throw new Error('Failed to get views');
   }
 
-  return await response.json();
+  viewsMap.set(slug, await response.json());
+
+  return viewsMap.get(slug);
 }
 
 export async function increaseViews(slug: string): Promise<{ views: number }> {
