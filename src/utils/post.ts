fd513c715b1560989c1065bbbb2f3e2edccfaff2
metacode22 <alohajune22@gmail.com>
2024-08-15 16:45:53 +0900

    feat: posts 가져올 때 updatedAt으로 sort

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -7,5 +7,7 @@ import { getContents } from './file';
 const CONTENTS_PATH = path.join(process.cwd(), 'src/contents/posts');
 
 export function getPosts() {
-  return getContents<PostMeta>(CONTENTS_PATH);
+  return getContents<PostMeta>(CONTENTS_PATH).sort(
+    (a, b) => new Date(b.meta.updatedAt).getTime() - new Date(a.meta.updatedAt).getTime(),
+  );
 }
