f351a9533c5b41ec54d25cd4fc74cfcb7147c28b
metacode22 <alohajune22@gmail.com>
2024-09-24 23:04:46 +0900

    feat: 방명록 등록 시 나에게 이메일 전송

--- app/api/guestbooks/route.ts
+++ app/api/guestbooks/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 import { prisma } from '@/src/utils/prisma';
 import { stringifyBigIntId } from '@/src/utils/stringify-big-int-id';
@@ -20,6 +21,25 @@ export async function POST(request: NextRequest) {
       message,
     },
   });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'alohajune22@gmail.com',
+      pass: process.env.GOOGLE_APP_PASSWORD,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: 'alohajune22@gmail.com',
+      to: 'alohajune22@gmail.com',
+      subject: '블로그에 방명록이 등록되었습니다.',
+      text: `${name}: ${message}`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
   return NextResponse.json({ guestbook: stringifyBigIntId(guestbook) });
 }
