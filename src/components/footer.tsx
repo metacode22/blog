cdec280c3d5faf65ac634ef929bc5850866e716e
metacode22 <alohajune22@gmail.com>
2024-10-18 09:19:25 +0900

    feat: footer의 링크들에 aria-label을 추가해 접근성 개선

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,12 +7,12 @@ import { Tooltip } from './tooltip';
 
 const LINKS = [
   {
-    name: 'github',
+    name: 'Github',
     href: 'https://github.com/metacode22',
     icon: <GithubIcon size={16} className='text-gray-400' />,
   },
   {
-    name: 'linkedin',
+    name: 'LinkedIn',
     href: 'https://www.linkedin.com/in/%EC%8A%B9%EC%A4%80-%EC%8B%A0-73602420a/',
     icon: <LinkedinIcon size={16} className='text-gray-400' />,
   },
@@ -35,7 +35,13 @@ export function Footer() {
             </span>
           </Tooltip>
           {LINKS.map(({ name, href, icon }) => (
-            <Link key={name} href={href} className='px-2 py-1' target='_blank' rel='noopener noreferrer'>
+            <Link
+              key={name}
+              href={href}
+              className='px-2 py-1'
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label={name}>
               {icon}
             </Link>
           ))}
