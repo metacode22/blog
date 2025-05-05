73ffbebf11b97c3bad6e7613f76060881d6f6b53
metacode22 <alohajune22@gmail.com>
2025-05-05 21:54:44 +0900

    chore: tailwind config에서 불필요한 코드 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,6 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        introduction:
-          'linear-gradient(rgb(0 0 0 / 0), white), url("/images/introduction-background.jpg")',
-      },
-    },
-  },
   plugins: [require('@tailwindcss/typography')],
 };
 export default config;
