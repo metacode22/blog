16c19550f89a309655725478069dd095df461fdc
metacode22 <alohajune22@gmail.com>
2026-03-01 00:29:04 +0900

    chore: CVE-2026-0969 이슈로 next-mdx-remote 버전 업데이트

--- src/utils/mdx.ts
+++ src/utils/mdx.ts
@@ -6,10 +6,6 @@ import rehypeSlug from 'rehype-slug';
 export async function serialize(content: string) {
   return serializeMDX(content, {
     mdxOptions: {
-      /**
-       * 다음 link와 같은 TS error로 인해 next-remote-mdx의 버전을 4.4.1로 사용
-       * @link https://github.com/hashicorp/next-mdx-remote/issues/423
-       */
       rehypePlugins: [
         rehypeSlug,
         [
@@ -22,7 +18,7 @@ export async function serialize(content: string) {
             },
           },
         ],
-        [rehypePrettyCode, { theme: 'dark-plus' }],
+        [rehypePrettyCode as any, { theme: 'dark-plus' }],
       ],
     },
   });
